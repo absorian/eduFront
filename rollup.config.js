1a4ebbbcd8ed5f9afba324758d2d3d3f7dd325bd
absorian <07221432bs@gmail.com>
2023-06-28 21:48:41 +0300

    typescript and stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import {nodeResolve} from "@rollup/plugin-node-resolve" // to resolve imports in src
+import typescript from '@rollup/plugin-typescript'; // to build compact bundles
+import { nodeResolve } from "@rollup/plugin-node-resolve" // to resolve imports in src
 import terser from '@rollup/plugin-terser'; // to build compact bundles
 
 export default {
-    input: "src/main.js",
+    input: "src/main.ts",
     output: {
         file: "dist/bundle.js"
     },
     plugins: [
+        typescript(),
         nodeResolve({ browser: true }),
         terser({
             compress: {
