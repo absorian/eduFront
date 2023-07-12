886f79a606daefb6eb43f5c1c7bb6da804b7825c
absorian <07221432bs@gmail.com>
2023-07-12 15:17:49 +0300

    add even more relative paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 			<Button appearence="transparent" href="./comic">Comic</Button>
 		</nav>
 		<Button href="./" appearence="transparent">
-			<img class="headerlogo" src="/icons/ism_logo.png" alt="logo" />
+			<img class="headerlogo" src="./icons/ism_logo.png" alt="logo" />
 		</Button>
 	</div>
 </header>
@@ -29,19 +29,19 @@
 
 	<footer class="shadow">
 		<Button appearence="transparent" href="https://github.com/absorian">
-			<img id="gh-link" class="contactico" src="/icons/github-mark.png" alt="github" />
+			<img id="gh-link" class="contactico" src="./icons/github-mark.png" alt="github" />
 		</Button>
 		<Button appearence="transparent" href="https://t.me/telegram">
-			<img id="tg-link" class="contactico" src="/icons/telegram.png" alt="telegram" />
+			<img id="tg-link" class="contactico" src="./icons/telegram.png" alt="telegram" />
 		</Button>
 		<Button appearence="transparent" href="mailto:ismagil@innopolis.university">
-			<img id="email" class="contactico" src="/icons/mail.png" alt="email" />
+			<img id="email" class="contactico" src="./icons/mail.png" alt="email" />
 		</Button>
 		<Button appearence="transparent" href="https://www.youtube.com/watch?v=y7r6PAkFRfU">
-			<img class="contactico" src="/icons/linkedin-logo.png" alt="linkedIn" />
+			<img class="contactico" src="./icons/linkedin-logo.png" alt="linkedIn" />
 		</Button>
 		<Button appearence="transparent" href="https://www.instagram.com/ism.gallery">
-			<img class="contactico" src="/icons/Instagram-icon.png" alt="instagram" />
+			<img class="contactico" src="./icons/Instagram-icon.png" alt="instagram" />
 		</Button>
 	</footer>
 </div>
